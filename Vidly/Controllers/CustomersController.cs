05ee86c4092833a23d3a7da2f186111e13fe8c6d
jlwolfla <jlwolfla@hotmail.com>
2019-04-27 21:57:41 -0500

    Saturday

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        List<Customer> customers = new List<Customer>
+        {
+            new Customer {Id = 1, Name = "John Wolfla"},
+            new Customer {Id = 2, Name = "Patrice Wolfla"},
+            new Customer {Id = 3, Name = "Joe Wolfla"},
+            new Customer {Id = 4, Name = "Patrice Wolfla"},
+            new Customer {Id = 5, Name = "Madison Wolfla"},
+            new Customer {Id = 6, Name = "Nick Wolfla"}
+        };
+
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+
+            return View(customers);
+        }
+
+
+        public ActionResult Details(int? Id)
+        {
+
+            var customer = (from _c in customers where _c.Id == Id select _c).FirstOrDefault();
+            return View(customer);
+        }
+
+
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,11 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        List<Movie> movies = new List<Movie>
+        {
+            new Movie {Id = 1, Name = "Die Hard"},
+            new Movie {Id = 2, Name = "The Matrix"},
+            new Movie {Id = 3, Name = "Walking Hard"},
+            new Movie {Id = 4, Name = "Rambo"}
+        };
+
+        public ActionResult Index()
+        {
+
+            return View(movies);
+
+        }
+
+
+        public ActionResult Details(int? Id)
+        {
+
+            var movie = (from _c in movies where _c.Id == Id select _c).FirstOrDefault();
+            return View(movie);
+        }
+
+
+
+
         // GET: Movies
         public ActionResult Random()
         {
@@ -18,7 +46,29 @@ namespace Vidly.Controllers
                 Name = "Die Hard"
             };
 
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "John Wolfla"},
+                new Customer {Name = "Patrice Wolfla"},
+                new Customer {Name = "Joe Wolfla"},
+                new Customer {Name = "Patrice Wolfla"},
+                new Customer {Name = "Madison Wolfla"},
+                new Customer {Name = "Nick Wolfla"}
+            };
+
+            var ViewModel = new RandomMovieViewModel
+            {
+                Customers = customers,
+                Movie = movie
+            };
+
+            return View(ViewModel);
+        }
+
+        [Route("movies/release/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int? year, int? month)
+        {
+            return Content(year + "/" + month);
         }
 
     }
